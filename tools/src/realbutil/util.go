61f40071c72e4db096b724e6b94d18cb7b54663b
Buffrr <contact@buffrr.dev>
2022-05-08 12:39:45 -0700

    Do initial refactoring for utils
    Renames & moves some common utils from other files to util.go.
    Prints stack trace for check calls

--- tools/src/realbutil/util.go
+++ tools/src/realbutil/util.go
@@ -3,27 +3,97 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime/debug"
+	"time"
 )
 
-func runAction(actionName, exePath string, args ...string) error {
-	fmt.Println(actionName)
-	return run(exePath, args...)
-}
-
-func runActionWithDir(actionName, dir, exePath string, args ...string) error {
+func run(actionName, dir, exePath string, args ...string) error {
 	fmt.Println(actionName)
 	cmd := newCmd(exePath, args...)
 	cmd.Dir = dir
-	return cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("%s: %v", cmd.String(), err)
+	}
+	return nil
 }
 
-func run(name string, args ...string) error {
-	return newCmd(name, args...).Run()
+func hasString(arr []string, s string) bool {
+	for _, c := range arr {
+		if c == s {
+			return true
+		}
+	}
+	return false
+}
+
+func fixOverrideTime(chromePath, beaconPath string) error {
+	chromeStat, err := os.Stat(chromePath)
+	if err != nil {
+		return err
+	}
+	beaconStat, err := os.Stat(beaconPath)
+	if err != nil {
+		return err
+	}
+	return compareFixOverrideTime(chromePath, beaconPath, chromeStat.ModTime(), beaconStat.ModTime())
+}
+
+func touchCCOverrides(chromePath, beaconPath string) {
+	_ = filepath.Walk(beaconPath, func(path string, overrideInfo fs.FileInfo, err error) error {
+		if err != nil || overrideInfo.IsDir() {
+			return nil
+		}
+
+		ext := filepath.Ext(path)
+		if ext != ".cc" && ext != ".h" && ext != ".mm" {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(beaconPath, path)
+		if err != nil {
+			log.Printf("[WARNING] failed getting relative path for chrome_src from: %v", err)
+			return nil
+		}
+
+		chromePath := filepath.Join(chromePath, relPath)
+		originalInfo, err := os.Stat(chromePath)
+		if err != nil {
+			log.Printf("[WARNING] no matching chrome override found for `%s` since `%s` isn't available: %v",
+				path, chromePath, err)
+			return nil
+		}
+		if err = compareFixOverrideTime(chromePath, path, originalInfo.ModTime(),
+			overrideInfo.ModTime()); err != nil {
+			log.Printf("[WARNING] %v", err)
+		}
+
+		return nil
+	})
+}
+
+func compareFixOverrideTime(chromePath, beaconPath string, chromeTime time.Time, beaconTime time.Time) error {
+	if chromeTime.Equal(beaconTime) {
+		return nil
+	}
+	// ensures both have the same time
+	fixTime := time.Now().Round(time.Second)
+	log.Printf("Touching override `%s`", chromePath)
+	if err := os.Chtimes(chromePath, fixTime, fixTime); err != nil {
+		return fmt.Errorf("failed changing mod time for chrome file `%s`:%v", chromePath, err)
+
+	}
+	if err := os.Chtimes(beaconPath, fixTime, fixTime); err != nil {
+		return fmt.Errorf("failed changing mod time for beacon file `%s`:%v", beaconPath, err)
+	}
+
+	return nil
 }
 
 func newCmd(name string, args ...string) *exec.Cmd {
@@ -52,9 +122,11 @@ func mustNotExist(path string) {
 	}
 }
 
-func must(err error) {
+func check(err error) {
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		debug.PrintStack()
+		os.Exit(1)
 	}
 }
 
